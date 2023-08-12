5a0e4aa0c5e32b9bc195184124283726bef11b1f
Dhanwin K B <141545073+Dhanwin-KB@users.noreply.github.com>
2023-08-12 18:59:44 +0530

    Update LABEVAL9.C

--- Lab Evaluation I/LABEVAL9.C
+++ Lab Evaluation I/LABEVAL9.C
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main()
 {
-	int cid,units;
+	int cid,units,tax;
 	char name[25];
-	float price;
+	float price=100;
 	price=0;
 	printf("Enter Customer Id:");
 	scanf("%d",&cid);
@@ -15,13 +15,30 @@ int main()
 	scanf("%d",&units);
 	fflush(stdin);
     if (units<=199)
-        {   price=units*1.20;   }
+        {   price=units*1.20;
+		    printf("\nRate: Rs 1.20 per unit");
+		}
     else if (units>200 && units<400)
-        {   price=units*1.50;   }
+        {   price=units*1.50;   
+			printf("\nRate: Rs 1.50 per unit");
+		}
     else if (units>=200 && units<400)
-        {   price=units*1.80;   }
+        {   price=units*1.80;   
+			printf("\nRate: Rs 1.80 per unit");
+		}
     else if (units<=600)
-        {   price=units*2.00;   }
-	printf("price=%f",price);
+        {   price=units*2.00;   
+			printf("\nRate: Rs 2.00 per unit");
+		}
+	printf("\n******BILL******");
+	printf("\nCustomer ID:%d",cid);
+	printf("\nUnits Consumed:%d",units);
+	printf("\nPrice:%.2f",price);
+	if (price>400)
+	{
+		tax=(15*price)/100;
+		price=price+tax;
+		printf("\nPrice after Surcharge of 15 percent:%.2f",price);	
+	}
 	return 0;
 }
